e2b512239647c24bedca775fc75c73b8bf899b9e
badal1771 <42205975+badal1771@users.noreply.github.com>
2018-11-17 03:42:17 +0530

    Update lab10_3.cpp

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,27 +1,30 @@
 #include <iostream>
 using namespace std;
 class box {
-public:
+public:  //using public in class
     //specification of box
-    double length;
-    double breath;
+    double length;    //length of box
+    double breath;    //breath of box
     double getarea(void){return length*breath;}
     double getperimeter(void){return length+breath;}};
     int main()
     {
         //declare box 1 of type box
-        box box1;
+        box box1;    //declare box 1 of type box
         //store the area value of the box
         double area=0;
         //store the perimeter value of box
         double perimeter=0;
         //data of box 1
-        box1.length=10;
-        box1.breath=20;
+        box1.length=10;   //inputing the length of box 1
+        box1.breath=20;   //inputing the length of box 2
         area=box1.getarea();
         perimeter=box1.getperimeter();
+        //displaying the area of box 1
         cout<<"the area of box 1 is "<<area<<endl;
+        //displaying the perimeter of box 1
         cout<<"the perimeter of box 1 is "<<perimeter<<endl;
+        //terminating the program
         return 0;
 
     }
